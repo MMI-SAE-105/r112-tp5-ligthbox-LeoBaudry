14ec483aa05e5b53ad2d35c2ca52531c3145e838
LeoBaudry <leo.baudry@edu.univ-fcomte.fr>
2024-12-04 08:54:36 +0100

    Correction CSS & début JS

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// *** LIGHTBOX ***
+
+const thumbnails = document.querySelectorAll('ul li img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImage = lightbox.querySelector('img');
+const lightboxBackdrop = document.querySelector('#lightbox::before');
+

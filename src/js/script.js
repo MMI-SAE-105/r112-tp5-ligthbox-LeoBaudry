7b30803269f5b1be566d63ba19c7c24d3c31e4fb
LeoBaudry <leo.baudry@edu.univ-fcomte.fr>
2024-12-04 09:04:14 +0100

    Corrections et ajouts CSS : Finalisation JS

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,21 @@
 const thumbnails = document.querySelectorAll('ul li img');
 const lightbox = document.getElementById('lightbox');
 const lightboxImage = lightbox.querySelector('img');
-const lightboxBackdrop = document.querySelector('#lightbox::before');
+const body = document.body;
 
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener('click', () => {
+    const fullImageUrl = thumbnail.dataset.fullImg; 
+    lightboxImage.src = fullImageUrl; 
+    lightbox.classList.add('open'); 
+    body.classList.add('lightbox-active'); 
+  });
+});
+
+function closeLightbox() {
+  lightbox.classList.remove('open'); 
+  body.classList.remove('lightbox-active'); 
+}
+
+lightbox.addEventListener('click', closeLightbox)
